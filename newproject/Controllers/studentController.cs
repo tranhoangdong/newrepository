181c93537aa06e66cd1c2dd35427c9eb01a536b3
tranhoangdong <88124107+tranhoangdong@users.noreply.github.com>
2021-08-06 13:26:27 +0700

    add controler

---
+++ newproject/Controllers/studentController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace newproject.Controllers
+{
+    public class studentController : Controller
+    {
+        // GET: student
+        public string Index()
+        {
+            return " tôi là trần hoàng đông ";
+        }
+    }
+}
