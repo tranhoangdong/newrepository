7a15c6b16d0de8b07d0617a049e416be93de2f0f
tranhoangdong <88124107+tranhoangdong@users.noreply.github.com>
2021-08-06 13:53:13 +0700

    add model

---
+++ newproject/Models/student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace newproject.Models
+{
+    public class student
+    {
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+        public int Age { get; set; }
+    }
+}
